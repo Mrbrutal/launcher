6e1ea0197557f8c3f68ab2f87945fb03cc1de50e
Mrbrutal <jost.bizjak@gmail.com>
2014-08-24 19:46:09 +0200

    All the assets for the launcher. Copyrighted material, and not to be distributed.

---
+++ pack.bat
@@ -0,0 +1,4 @@
+mvn clean package
+
+pack200 -g "D:\Minecraft modding\Launcher\Project\launcher\target\4.2.6.jar.pack" "D:\Minecraft modding\Launcher\Project\launcher\target\SKCraftLauncher.jar"
+@pause
